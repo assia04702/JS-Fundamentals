1c7c4e7bec6b3865248aa6554519166d76c5ae1a
ASSIA GRAM <assiagram03@gmail.com>
2025-09-14 17:35:46 +0100

    Add script that prints 3 lines using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and join lines with '\n'
+console.log(languages.join('\n'));
