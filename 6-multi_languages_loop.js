a7a1212718fc045db058180d87500a0b035a36ed
ASSIA GRAM <assiagram03@gmail.com>
2025-09-15 01:11:40 +0100

    Add 6-multi_languages_loop.js: print 3 languages using loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,3 +1,4 @@
+cat > 6-multi_languages_loop.js <<'EOF'
 #!/usr/bin/node
 
 const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
@@ -5,24 +6,5 @@ const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < messages.length; i++) {
   console.log(messages[i]);
 }
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let output = ""; // لتجميع جميع الأسطر
+EOF
 
-for (let i = 0; i < languages.length; i++) {
-  output += languages[i];
-  if (i < languages.length - 1) output += "\n"; // نزيد سطر جديد إلا مازال باقي عناصر
-}
-
-console.log(output);
-// Array of strings
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-// Loop through the array and join lines with '\n'
-console.log(languages.join('\n'));
-#!/usr/bin/node
-
-const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const msg of messages) {
-  console.log(msg);
-}
