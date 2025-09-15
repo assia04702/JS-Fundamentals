18b6a57b571c62e0ab401efc4399b8d3ad58e6d0
ASSIA GRAM <assiagram03@gmail.com>
2025-09-15 01:16:27 +0100

    Fix shebang and add loop script

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,3 @@
-cat > 6-multi_languages_loop.js <<'EOF'
 #!/usr/bin/node
 
 const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
@@ -6,5 +5,5 @@ const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < messages.length; i++) {
   console.log(messages[i]);
 }
-EOF
+
 
